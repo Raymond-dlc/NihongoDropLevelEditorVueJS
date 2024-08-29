b731f61dbec56085ef3a4863f1bc5510dad043b8
Raymond-dlc <153617766+Raymond-dlc@users.noreply.github.com>
2024-08-29 20:52:08 +0200

    Make padding more responsive

--- src/views/ChapterView.vue
+++ src/views/ChapterView.vue
@@ -1,7 +1,7 @@
 <script lang="ts"></script>
 
 <template>
-  <main class="flex flex-1 flex-col bg-grey-100 p-8">
+  <main class="flex flex-1 flex-col bg-grey-100 p-8 pl-32 md:p-8">
     <h1>Chapter Page</h1>
     <br />
     <p>This is the chapter page!</p>
--- src/views/WordsView.vue
+++ src/views/WordsView.vue
@@ -1,7 +1,7 @@
 <script lang="ts"></script>
 
 <template>
-  <main class="flex flex-col flex-1 bg-grey-100 p-8">
+  <main class="flex flex-col flex-1 bg-grey-100 p-8 pl-32 md:p-8">
     <h1>Words Page</h1>
     <br/>
     <p>This is the words page!</p>
